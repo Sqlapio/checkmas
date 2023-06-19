50f6c28ae395936c3e4749f684d1325369d7cc09
Wilfredo <wpalencia@sqlapio.com>
2023-06-19 15:38:39 -0400

    Correciones ortografica dashboard

--- resources/views/livewire/iaim/art-salida.blade.php
+++ resources/views/livewire/iaim/art-salida.blade.php
@@ -53,7 +53,7 @@ $ordenes = IaimOrdenTrabajo::all();
                     <th scope="col" class="py-3.5 px-4 text-sm font-semibold text-left rtl:text-right text-white">
                         <div class="flex items-center gap-x-3">
                             <button class="flex items-center gap-x-2">
-                                <span class="ml-1">ID Aticulo</span>
+                                <span class="ml-1">ID Artículo</span>
                                 <svg class="h-3" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z" fill="currentColor" stroke="currentColor" stroke-width="0.1" />
                                     <path d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z" fill="currentColor" stroke="currentColor" stroke-width="0.1" />
